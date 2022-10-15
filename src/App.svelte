85aaade2ed9147c71f6f257e182a0176e423f31b
Piotr Wicijowski <piotr@wicijowski.pl>
2022-10-16 01:53:30 +0200

    Remove debug

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,12 @@
 <script>
-    import { Router, Link, Route } from "svelte-routing";
+    import { Route, Router } from "svelte-routing";
     import Navigation from "./components/Navigation.svelte";
     import Home from "./routes/Home.svelte";
     import Manual from "./routes/Manual.svelte";
     import Selected from "./routes/Selected.svelte";
     import Stats from "./routes/Stats.svelte";
-    import { housesStore } from "./services/stores";
 
     export let url = "";
-
-    housesStore.subscribe(value => {
-        console.log(JSON.stringify(value));
-    });
 </script>
 
 <Router {url}>
